29e2ba730590844e9e9f03e06923efa6465e173f
Adam Fraser <Fraser.j.adam@gmail.com>
2021-05-02 20:50:24 -0400

    Update AHDump.py

--- AHDump/AHDump.py
+++ AHDump/AHDump.py
@@ -6,61 +6,61 @@ import pandas as pd
 
 def main():
     
-    realmJSON = open('RealmList.json', encoding="utf8")
-    realm_data = json.load(realmJSON)
+    realm_json = open('RealmList.json', encoding="utf8")
+    realm_data = json.load(realm_json)
     realm_slug = []
 
     for x in range(len(realm_data['results'])):
         for y in range(len(realm_data['results'][x]['data']['realms'])):
                 realm_slug.append(realm_data['results'][x]['data']['realms'][y]['slug'])
 
-    def get_realmID(realm_name):
+    def get_realm_id(realm_name):
 
         for x in range(len(realm_data['results'])):
             for y in range(len(realm_data['results'][x]['data']['realms'])):
                 if realm_data['results'][x]['data']['realms'][y]['slug'] == realm_name:
-                    realmID = str(realm_data['results'][x]['data']['id'])
+                    realm_id = str(realm_data['results'][x]['data']['id'])
 
                     init = CRS()
                     cid = init.cfg.token_client_id
                     sec = init.cfg.token_secret
-                    realmAPI = 'https://us.api.blizzard.com/data/wow/connected-realm/' + realmID + '/auctions?namespace=dynamic-us&locale=en_US&access_token='
+                    realm_api = 'https://us.api.blizzard.com/data/wow/connected-realm/' + realm_id + '/auctions?namespace=dynamic-us&locale=en_US&access_token='
 
                     def get_access_token(c_id, c_secret, region = 'us'):
                         data = {'grant_type': 'client_credentials'}
                         response = requests.post('https://%s.battle.net/oauth/token' % region, data=data, auth=(c_id, c_secret))
                         return response.json()
 
-                    def server_US(token):
-                        search =  realmAPI + token
+                    def server_us(token):
+                        search =  realm_api + token
                         response = requests.get(search)
                         return response.json()["auctions"]
 
                     response = get_access_token(cid, sec)
                     token = response['access_token']
-                    server_AHdata = server_US(token)
+                    server_auction_data = server_us(token)
 
-                    AHdump_df = pd.DataFrame(server_AHdata)
-                    AHdump_df = AHdump_df.rename(columns={"id": "auction_id",})
-                    AHdump_df = pd.concat([AHdump_df.drop(['item'], axis=1), AHdump_df['item'].apply(pd.Series).filter(items=['id'])], axis=1)
-                    AHdump_df = AHdump_df.rename(columns={"id": "item_id",})
+                    auction_dump_df = pd.DataFrame(server_auction_data)
+                    auction_dump_df = auction_dump_df.rename(columns={"id": "auction_id",})
+                    auction_dump_df = pd.concat([auction_dump_df.drop(['item'], axis=1), auction_dump_df['item'].apply(pd.Series).filter(items=['id'])], axis=1)
+                    auction_dump_df = auction_dump_df.rename(columns={"id": "item_id",})
 
-                    AHdump_df['cost'] = (AHdump_df['unit_price'].fillna(0) + AHdump_df['buyout'].fillna(0)).astype(str)
-                    AHdump_df['price_gold']=AHdump_df['cost'].astype(str).str[:-6]
-                    AHdump_df['price_silver']=AHdump_df['cost'].astype(str).str[-6:-4]
-                    AHdump_df['price_copper']=AHdump_df['cost'].astype(str).str[-4:-2]
+                    auction_dump_df['cost'] = (auction_dump_df['unit_price'].fillna(0) + auction_dump_df['buyout'].fillna(0)).astype(str)
+                    auction_dump_df['price_gold']=auction_dump_df['cost'].astype(str).str[:-6]
+                    auction_dump_df['price_silver']=auction_dump_df['cost'].astype(str).str[-6:-4]
+                    auction_dump_df['price_copper']=auction_dump_df['cost'].astype(str).str[-4:-2]
 
                     filename = (str(realm_name) + '_AHDump_' + str(datetime.date.today()) + '.csv')
-                    AHdump_df.to_csv(filename, index=False)
+                    auction_dump_df.to_csv(filename, index=False)
 
-    def input_realmID():
+    def input_realm_id():
             user_input = str(input('Input Realm name:')).lower()
             if user_input in realm_slug:
-                get_realmID(user_input)
+                get_realm_id(user_input)
             else:
                 print('Invalid realm name, try again')
 
-    input_realmID()
+    input_realm_id()
 
 if __name__ == "__main__":
     main()
