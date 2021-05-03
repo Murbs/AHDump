64f773d0e3051bbddaa2609acf92d4c2161654b3
Adam Fraser <Fraser.j.adam@gmail.com>
2021-05-03 06:00:21 -0400

    Update CRS.py

--- AHDump/CRS.py
+++ AHDump/CRS.py
@@ -1,10 +1,11 @@
+import os
+
 class Config:
 
     def __init__(self):
-        self.token_client_id = 'YOUR CLIENT ID GOES HERE'
-        self.token_secret = 'YOUR SECRET GOES HERE'
+        self.token_client_id = os.environ['client_id']
+        self.token_secret = os.environ['secret']
         
 class CRS:
     def __init__(self):
         self.cfg = Config()
-        
