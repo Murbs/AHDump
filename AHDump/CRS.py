a8af7aa56394bd2656ffd2a0cc02fc8ec69b8e75
Adam Fraser <Fraser.j.adam@gmail.com>
2021-05-14 06:24:27 -0400

    Update CRS.py
    Trimmed wrapper

--- AHDump/CRS.py
+++ AHDump/CRS.py
@@ -5,7 +5,3 @@ class Config:
     def __init__(self):
         self.token_client_id = os.environ['client_id']
         self.token_secret = os.environ['secret']
-        
-class CRS:
-    def __init__(self):
-        self.cfg = Config()
